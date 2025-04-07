bb8a02b1d75c09d18e561b9fcba2b96f03e12496
Yifeng Li <tomli@tomli.me>
2025-04-07 09:57:16 +0000

    doc: rewrite impedance transformation section.
    Signed-off-by: Yifeng Li <tomli@tomli.me>

--- python/doc/Tutorials/raw-data/Parallel_Plate_Capacitor_Waveguide/half-wave-transformer.py
+++ python/doc/Tutorials/raw-data/Parallel_Plate_Capacitor_Waveguide/half-wave-transformer.py
@@ -11,20 +11,22 @@ import schemdraw
 import schemdraw.elements as elm 
 
 
-def calc(wavelength):
+def calc(wavelength, zport):
     freq = skrf.frequency.Frequency(start=1e9, stop=1e9, unit='Hz', npoints=1)
 
-    media600 = DefinedAEpTandZ0(freq, z0=600)
-    line600 = media600.line(d=360 * wavelength, unit='deg', z0_port=50)
-    resistor50 = media600.resistor(50) ** media600.short()
-    network = line600 ** resistor50
-    network.renormalize(50)
+    media = DefinedAEpTandZ0(freq, z0=50)
+    line = media.line(d=360 * wavelength, unit='deg', z0_port=50)
+    resistor = media.resistor(600) ** media.short()
+    network = line ** resistor
+    print(network.s, network.z)
+    if zport != 50:
+        network.renormalize(zport)
 
     return [network.s[:,0,0][0], network.z[:,0,0][0]]
 
 
-def draw(wavelength, output):
-    gamma, z = calc(wavelength)
+def draw(wavelength, zport, output):
+    gamma, z = calc(wavelength, zport)
     print(gamma, z)
 
     with schemdraw.Drawing(file=output) as d:
@@ -33,7 +35,7 @@ def draw(wavelength, output):
         
         zg = (elm.Resistor().right()
                            .label(r'$Z_G$', loc='top')
-                           .label(r'$50\Omega$', loc="bottom")
+                           .label(r'$%d\Omega$' % zport, loc="bottom")
         )
         d += zg
         
@@ -42,14 +44,23 @@ def draw(wavelength, output):
             gamma_deg = 0
         else:
             gamma_deg = degrees(phase(gamma))
+            if -0.1 < gamma_deg < 0.1:
+                gamma_deg = 0
+
+        if abs(z) < 1e-10:
+            phase_deg = 0
+        else:
+            phase_deg = degrees(phase(z))
+            if -0.1 < phase_deg < 0.1:
+                phase_deg = 0
 
         label = "$\Gamma= %.1f\\angle%.1f^\circ$\n$Z_{in} = %.1f\\angle%.1f^\circ \Omega$" % (
-            abs(gamma), gamma_deg, abs(z), degrees(phase(z))
+            abs(gamma), gamma_deg, abs(z), phase_deg
         )
         print(
-            "A 50 Ω transmitter is connected to a 50 Ω receiver via a "
-            "600 Ω transmission line with a length of %.1fλ, its input reflection "
-            "coefficient is %.1f∠%.1f°, its input impedance is %.1f∠%.1f° Ω." % (wavelength, abs(gamma), gamma_deg, abs(z), degrees(phase(z)))
+            "A %d Ω transmitter is connected to a 600 Ω receiver via a "
+            "50 Ω transmission line with a length of %.1fλ, its input reflection "
+            "coefficient is %.1f∠%.1f°, its input impedance is %.1f∠%.1f° Ω." % (zport, wavelength, abs(gamma), gamma_deg, abs(z), phase_deg)
         )
 
         d += (elm.Dot().right()
@@ -57,14 +68,14 @@ def draw(wavelength, output):
         )
         
         d += (elm.Coax().right()
-                       .label(r'$600\Omega$', loc='top')
+                       .label(r'$50\Omega$', loc='top')
                        .label(r'$%.1f\lambda$' % wavelength, loc='bottom')
         )
         
         
         zl = (elm.Resistor().down()
                            .label(r'$Z_L$', loc='top')
-                           .label(r'$50\Omega$', loc="bottom")
+                           .label(r'$600\Omega$', loc="bottom")
         )
         d += zl
         
@@ -72,6 +83,9 @@ def draw(wavelength, output):
         d += elm.Line().left().tox(source.start)
     
 
-draw(0.2, "half-wave-1.svg")
-draw(0.4, "half-wave-2.svg")
-draw(0.5, "half-wave-3.svg")
+draw(0.2, 50, "half-wave-1.svg")
+draw(0.4, 50, "half-wave-2.svg")
+draw(0.5, 50, "half-wave-3.svg")
+draw(0.2, 600, "half-wave-4.svg")
+draw(0.4, 600, "half-wave-5.svg")
+draw(0.5, 600, "half-wave-6.svg")
